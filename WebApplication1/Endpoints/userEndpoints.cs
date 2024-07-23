7553b5f9dd8bb36970d684b0ea51708938237c86
Onixfff <danilnikonov01@mail.ru>
2024-07-23 13:31:13 +0200

    Добавьте файлы проекта.

---
+++ WebApplication1/Endpoints/userEndpoints.cs
@@ -0,0 +1,24 @@
+﻿namespace WebApplication1.Endpoints
+{
+    public static class userEndpoints
+    {
+        public static IEndpointRouteBuilder MapUsersEndpoinds (this IEndpointRouteBuilder app)
+        {
+            app.MapPost("register", Register);
+
+            app.MapPost("login", Login);
+
+            return app;
+        }
+
+        private static async Task<IResult> Register()
+        {
+            return Results.Ok();
+        }
+
+        private static async Task<IResult> Login()
+        {
+            return Results.Ok();
+        }
+    }
+}
---
+++ WebApplication1/Services/UserService.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Services
+{
+    public class UserService
+    {
+        public async Task Register(string userName, string email, string passwordHash)
+        {
+            
+        }
+
+
+    }
+}
